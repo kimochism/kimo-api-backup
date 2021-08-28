9e0e35b616f351588dd09670a640c42c2e96f3fd
Luska <ylucasx3@gmail.com>
2021-08-28 15:03:03 -0300

    fix(cors)

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ global.XMLHttpRequest = require("xhr2");
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors({ credentials: true, origin: ['http://localhost:3000', 'htpps://kimochism.com'] });
+  app.enableCors();
   await app.listen(process.env.PORT || 3333);
   firebase.initializeApp(firebaseConfig);
 }
