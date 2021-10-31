90d62215faf30247a3812aa8728e045f8224a03c
Lucas Ariel <ylucasx3@gmail.com>
2021-10-31 20:35:38 -0300

    fix(url to confirmEmail)

--- src/utils/confirmEmailLink.ts
+++ src/utils/confirmEmailLink.ts
@@ -1,8 +1,10 @@
 import { redis } from '../redis';
 import { v4 } from 'uuid';
 
+const { API_URL } = process.env;
+
 export const confirmEmailLink = async (userId: string) => {
     const id = v4();
     await redis.set(id, userId, 'ex', 60 * 60 * 15);
-    return `http://localhost:3000/verifyEmail/${id}`;
+    return `${API_URL}/verifyEmail/${id}`;
 }
